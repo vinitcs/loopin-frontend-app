69e1a3705f15b9db759cd8390a9065a75d3a6dcf
vinitcs <vinitchavan1234@gmail.com>
2026-02-19 18:06:31 +0530

    header alignments fixed

--- src/components/Custom/Header.js
+++ src/components/Custom/Header.js
@@ -1,15 +1,17 @@
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {colors} from '../../theme/colors/colors';
 import {Bell, EllipsisVertical} from 'lucide-react-native';
 import {useNavigation} from '@react-navigation/native';
 import GoBackIcon from '../GoBackIcon/GoBackIcon';
 import {fonts} from '../../theme/fonts/fonts';
+import {colors} from '../../theme/colors/colors';
 
 const Header = ({
   showAppLogo = true,
   showBell = false,
   showEllipsisVertical = false,
+  showTitle = false,
+  titleContent = '',
   bottomSheetRef,
 }) => {
   const navigation = useNavigation();
@@ -23,40 +25,49 @@ const Header = ({
   };
   return (
     <View style={styles.container}>
-      {/* Left side logo */}
-      {showAppLogo ? (
-        <Text
-          style={{
-            fontSize: 18,
-            fontFamily: fonts.Bold,
-            color: colors.Primary,
-          }}>
-          LoopIn
-        </Text>
-      ) : (
-        <GoBackIcon />
-      )}
+      {/* Left section */}
+      <View style={styles.leftSection}>
+        {showAppLogo ? (
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: fonts.Bold,
+              color: colors.Primary,
+            }}>
+            LoopIn
+          </Text>
+        ) : (
+          <GoBackIcon />
+        )}
+      </View>
 
-      {/* Right side group */}
-      <View style={styles.sectionGroup}>
-        <View style={styles.sectionPair}>
-          {showBell && (
-            <TouchableOpacity
-              style={styles.actionFeatureIcon}
-              onPress={navigateTo}>
-              <Bell color={colors.Primary} fill={colors.Primary} size={20} />
-            </TouchableOpacity>
-          )}
+      {/* Center section */}
+      <View style={styles.centerSection}>
+        {showTitle && <Text style={styles.title}>{titleContent}</Text>}
+      </View>
 
-          {showEllipsisVertical && (
-            <TouchableOpacity onPress={openSettingSheet}>
-              <EllipsisVertical
-                color={colors.Primary}
-                fill={colors.Primary}
-                size={20}
-              />
-            </TouchableOpacity>
-          )}
+      {/* Right section */}
+      <View style={styles.rightSection}>
+        <View style={styles.sectionGroup}>
+          <View style={styles.sectionPair}>
+            {showBell && (
+              <TouchableOpacity
+                style={styles.actionFeatureIcon}
+                onPress={navigateTo}>
+                <Bell color={colors.Primary} fill={colors.Primary} size={20} />
+              </TouchableOpacity>
+            )}
+
+            {showEllipsisVertical && (
+              <TouchableOpacity onPress={openSettingSheet}>
+                <EllipsisVertical
+                  color={colors.Primary}
+                  fill={colors.Primary}
+                  size={20}
+                />
+              </TouchableOpacity>
+            )}
+          </View>
         </View>
       </View>
     </View>
@@ -67,12 +78,30 @@ export default Header;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.Background1,
+    // backgroundColor: colors.Highlight1,
     height: 60,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
     paddingHorizontal: 15,
+  },
+  leftSection: {
+    // backgroundColor: colors.Accent, // testing 
+    width: 60,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  centerSection: {
+    // backgroundColor: colors.Outline, // testing
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rightSection: {
+    // backgroundColor: colors.Error, // testing
+    width: 60,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   sectionGroup: {
     flexDirection: 'row',
@@ -81,9 +110,14 @@ const styles = StyleSheet.create({
   sectionPair: {
     flexDirection: 'row',
     alignItems: 'center',
-    //     marginRight: 14,
   },
   actionFeatureIcon: {
     padding: 4,
+  },
+  title: {
+    fontSize: 14,
+    fontFamily: fonts.SemiBold,
+    color: colors.Text1,
+    textAlign: 'center',
   },
 });
